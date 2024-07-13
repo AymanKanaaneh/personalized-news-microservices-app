51a11c6c0bc5c641e2d50989029e303dc9f94a3c
Ayman Hussein <ayman.kanaaneh@gmail.com>
2024-07-13 03:43:19 +0300

    fix(preferences): correct PUT user preferences endpoint implementation

--- userAccessor/src/controllers/userController.js
+++ userAccessor/src/controllers/userController.js
@@ -11,8 +11,10 @@ const registerUser = async (req, res) => {
     
 const updateUserPreferences = async (req, res) => {
   try {
-    user = await userService.updateUserPreferences(req.params.userId, req.body.preferences);
-    res.status(200).json(user);
+
+    console.log(req.body);
+    updatedUserPreferences = await userService.updateUserPreferences(req.body);
+    res.status(200).json(updatedUserPreferences);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
